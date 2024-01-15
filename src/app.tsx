ff36b82e73f8f34fb76458c35cf69940e4a611b3
Nihar Hegde <niharhegde163@gmail.com>
2024-01-15 20:21:01 +0530

    adding an input form

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,35 @@
 import React from "react";
+import { Button } from "./components/button";
+import { Input } from "./components/input";
+import { z } from "zod";
 
 const App: React.FC = () => {
-  const [count, setCount] = React.useState(0);
+  const [size, setSize] = React.useState<number | null>(null);
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const parseData = z
+      .number({
+        coerce: true,
+      })
+      .positive()
+      .min(1)
+      .safeParse(formData.get("grid"));
+
+    if (parseData.success) {
+      setSize(parseData.data);
+    }
+  }
 
   return (
     <section className="flex flex-col w-full h-screen items-center justify-center">
-      <h1 className="text-2xl mb-2">Vite + React</h1>
-
-      <div className="flex gap-2 items-center flex-col">
-        <button
-          type="button"
-          className="bg-primary hover:bg-primary/90 text-primary-foreground font-bold py-2 px-4 rounded"
-          onClick={() => {
-            setCount((prev) => prev + 1);
-          }}>
-          count is {count}
-        </button>
-
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <label className="flex flex-col">
+          Grid Size
+          <Input type="number" min="1" max="100" name="grid" />
+        </label>
+        <Button>Create Grid</Button>
+      </form>
     </section>
   );
 };
